eee43b6c30ebf88aa64004743ba20410170d3569
austinslee <alee235@ucr.edu>
2024-03-16 22:44:08 -0700

    Implemented returnCurrLTS()
    Implemented returnCurrLTS() for derived class

--- fetcher.cpp
+++ fetcher.cpp
@@ -35,36 +35,81 @@ std::vector<std::string> SimpleStreamsFormatFetcher::returnSupportedReleases()  
 	}
 	return toReturn;
 }
-	std::string SimpleStreamsFormatFetcher::returnCurrLTS() {
-		return "";
-	}
 
-	//given release_title, return the sha256 of the disk1.img
-	//Noticed that some of the Ubuntu releases don't have disk1.img. For example, Ubuntu 10.10
-	std::string SimpleStreamsFormatFetcher::returnDisk1Sha256(std::string release) {
-		std::string toReturn = "";
-		CURL *curl = curl_easy_init();
-		CURLcode res;
-		std::string responseBuffer;
-		if(curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json");
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writecb);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
-			res = curl_easy_perform(curl);
-			if(res != CURLE_OK) {
-				printf("transfer fail: %s\n", curl_easy_strerror(res));
-			}
-			nlohmann::json jsonData = nlohmann::json::parse(responseBuffer);
-	                nlohmann::json jsonProductArray = jsonData["products"];
-        	        for (const auto& product : jsonProductArray) {
-                	        if (product["release_title"].get<std::string>() == release && product["arch"].get<std::string>() == "amd64") {
-					for(const auto& key : product["versions"])  {
-						toReturn = key["items"]["disk1.img"]["sha256"].get<std::string>();
-					}
-        	                }
-                	}
-	                curl_easy_cleanup(curl);
+//Not sure how to find the Current Ubuntu LTS version. Working under the assumption that it is the latest version that has LTS in release_title
+//return the Ubuntu release_title with LTS in the release_title and highest version number.
+std::string SimpleStreamsFormatFetcher::returnCurrLTS() {
+	CURL *curl = curl_easy_init();
+	CURLcode res;
+	std::string responseBuffer;
+	std::string toReturn = "ERROR: Not Found\n";
+	double maxVersion = 0.0;
+	double temp;
+
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writecb);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
+		res = curl_easy_perform(curl);
+		if(res != CURLE_OK){
+			printf("transfer fail: %s\n", curl_easy_strerror(res));
 		}
-		return toReturn;
+		nlohmann::json jsonData = nlohmann::json::parse(responseBuffer);
+		nlohmann::json jsonProductArray = jsonData["products"];
+
+/* (str.find(substring) != std::string::npos) {
+	std::cout << "String contains the substring \"" << substring << "\"" << std::endl;
+}*/
+		for (const auto& product : jsonProductArray) {
+			//checks if arch == amd64, version number is the latest, and contains LTS in release title
+			if (product["arch"].get<std::string>() == "amd64") {
+				if(product["release_title"].get<std::string>().find("LTS") != std::string::npos) {
+					temp = std::stod(product["version"].get<std::string>());
+					if(temp > maxVersion) {
+						toReturn = product["release_title"].get<std::string>();
+					}
+				}
+			}
+		}
+	curl_easy_cleanup(curl);
 	}
+	return toReturn;
+}
+
+
+
+//given release_title, return the sha256 of the disk1.img
+//Some of the Ubuntu releases don't have disk1.img. For example, Ubuntu 10.10
+//Some of the Ubuntu releases have multiple disk1.img. For example Ubuntu 23.10
+std::string SimpleStreamsFormatFetcher::returnDisk1Sha256(std::string release) {
+	//std::string toReturn = "Ubuntu release_title not found";
+	std::string toReturn = "";
+	CURL *curl = curl_easy_init();
+	CURLcode res;
+	std::string responseBuffer;
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://cloud-images.ubuntu.com/releases/streams/v1/com.ubuntu.cloud:released:download.json");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writecb);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
+		res = curl_easy_perform(curl);
+		if(res != CURLE_OK) {
+			printf("transfer fail: %s\n", curl_easy_strerror(res));
+		}
+		nlohmann::json jsonData = nlohmann::json::parse(responseBuffer);
+                nlohmann::json jsonProductArray = jsonData["products"];
+       	        for (const auto& product : jsonProductArray) {
+               	        if (product["release_title"].get<std::string>() == release && product["arch"].get<std::string>() == "amd64") {
+				for(const auto& key : product["versions"])  {
+					if(key["items"].contains("disk1.img")) {
+						toReturn = key["items"]["disk1.img"]["sha256"].get<std::string>();
+					} else {
+						toReturn = "ERROR: disk1.img not found";
+					}
+				}
+       	                }
+               	}
+                curl_easy_cleanup(curl);
+	}
+	return toReturn;
+}
 
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
 	std::string option;
 	std::cout << "Welcome to SimpleStreamsFormatFetcher CLI\n" << "The following commands are:\n";
 	while(1) {
-		std::cout << "1. Return a list of all currently supported Ubuntu releases.\n2. Return the current Ubuntu LTS version.\n3. Return the sha256 of the disk1.img item of a given Ubuntu release.\n4. Break\n";
+		std::cout << "1. Return a list of all currently supported Ubuntu releases.\n2. Return the current Ubuntu LTS version.\n3. Return the sha256 of the disk1.img item of a given Ubuntu release.\n4. Exit\n";
 		std::cout << "To run command please enter the number associated with desired command.\n";
 		std::cin >> option;
 		if(option == "1") {
@@ -16,11 +16,13 @@ int main() {
 				std::cout << list.at(i) << "\n";
 			}
 		} else if (option == "2") {
+			std::cout << test.returnCurrLTS() << "\n";
 		} else if (option == "3") {
-			std::cout << "Please enter release_title\n";
-			std::cin >> option;
+			std::cout << "Please enter the release_title: ";
+			std::cin.ignore();
+			std::getline(std::cin, option);
 			std::cout << test.returnDisk1Sha256(option) << "\n";
-		} else if (option == "break") {
+		} else if (option == "4") {
 			break;
 		}
 	}
